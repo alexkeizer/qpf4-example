eccd9675de96983bde07244fc6d9651d5e54db95
Alex Keizer <alex@keizer.dev>
2023-03-30 22:31:05 +0200

    Change main to print incrementally

--- Main.lean
+++ Main.lean
@@ -4,14 +4,14 @@ def main : IO Unit := do
   let nats := QpfStream.naturals
   let mut evenNats := nats.add nats
 
-  let list := evenNats.toList 1000
-  IO.println list
+  -- let list := evenNats.toList 1000
+  -- IO.println list
 
 
-  -- for i in List.range 10000 do
-  --   let ⟨(hd : Nat), tl⟩ := evenNats.dest
-  --   IO.println hd
-  --   evenNats := tl
+  for i in List.range 1000 do
+    let ⟨(hd : Nat), tl⟩ := evenNats.dest
+    IO.println hd
+    evenNats := tl
 
   return
 
