d9b2fe8b29c6a145452e1f9e73d1554f6989889d
Alex Keizer <alex@keizer.dev>
2023-03-30 22:29:49 +0200

    Add QpfStream examples

--- Main.lean
+++ Main.lean
@@ -1,5 +1,17 @@
 import Scratch
 
 def main : IO Unit := do
+  let nats := QpfStream.naturals
+  let mut evenNats := nats.add nats
+
+  let list := evenNats.toList 1000
+  IO.println list
+
+
+  -- for i in List.range 10000 do
+  --   let ⟨(hd : Nat), tl⟩ := evenNats.dest
+  --   IO.println hd
+  --   evenNats := tl
+
   return
 
--- Scratch/Examples.lean
+++ Scratch/Examples.lean
@@ -1,6 +1,47 @@
 import Qpf
 
+open MvQpf
+
 
 data QpfList α 
   | nil
   | cons : α → QpfList α → QpfList α
+
+
+codata QpfStream α where
+  | mk : α → QpfStream α → QpfStream α
+
+
+def QpfStream.mk (hd : α) (tl : QpfStream α) : QpfStream α :=
+  Cofix.mk ⟨hd, tl⟩
+
+
+/-- The stream `0,0,0,...` -/
+def QpfStream.zeroes : QpfStream Nat :=
+  Cofix.corec (fun _ => 
+    Shape.mk (0 : Nat) ()
+  ) ()
+
+/-- The stream `0,1,2,3,4,...` -/
+def QpfStream.naturals : QpfStream Nat :=
+  Cofix.corec (fun (i : Nat) => 
+    Shape.mk (i : Nat) (i + 1 : Nat)
+  ) 0
+
+
+/-- Add two streams together -/
+def QpfStream.add (as bs : QpfStream Nat) : QpfStream Nat :=
+    Cofix.corec (fun ⟨as, bs⟩ => 
+      let ⟨(a : Nat), as⟩ := Cofix.dest as;
+      let ⟨(b : Nat), bs⟩ := Cofix.dest bs;
+      Shape.mk (a + b : Nat) (as, bs)
+    ) (as, bs)
+
+
+
+def QpfStream.toList : QpfStream α → Nat → List α 
+  | _, 0   => []
+  | as, n+1 =>  
+    let ⟨hd, tl⟩ := as.dest
+    hd :: (toList tl n)
+
