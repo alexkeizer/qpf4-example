446303ea323fe22f93de592694cb9269bc5144cc
Alex Keizer <alex@keizer.dev>
2023-04-05 21:53:45 +0200

    Chore: bump versino

--- lakefile.lean
+++ lakefile.lean
@@ -10,4 +10,4 @@ lean_lib Examples
 @[default_target]
 lean_exe Main
 
-require Qpf from git "https://github.com/alexkeizer/qpf4.git" @ "512160641c9cfc186cc8fac9903b1c42cdc84755"
+require Qpf from git "https://github.com/alexkeizer/qpf4.git" @ "1055bd0"
